370aaa1e7f1a011b83a50a21302616b163dd9644
danielemorra98 <morradaniele@ymail.com>
2023-01-30 16:35:19 +0100

    new function to retrieve the hierarchyMap
    Signed-off-by: danielemorra98 <morradaniele@ymail.com>

--- Gems/ROS2/Code/Source/RobotImporter/URDFMetadataComponent.cpp
+++ Gems/ROS2/Code/Source/RobotImporter/URDFMetadataComponent.cpp
@@ -10,6 +10,8 @@
 #include <AzCore/Debug/Trace.h>
 #include <AzCore/std/containers/unordered_map.h>
 #include <AzCore/Serialization/SerializeContext.h>
+#include <AzCore/Serialization/EditContext.h>
+
 
 namespace ROS2
 {
@@ -26,7 +28,7 @@ namespace ROS2
         if (AZ::SerializeContext* serialize = azrtti_cast<AZ::SerializeContext*>(context))
         {
             serialize->Class<URDFMetadataComponent, AZ::Component>()
-                ->Version(2)
+                ->Version(0)
                 ->Field("HierarchyMap", &URDFMetadataComponent::m_hierarchyMap);
         }
     }
@@ -36,5 +38,9 @@ namespace ROS2
         m_hierarchyMap = hierarchyMap;
     }
 
+    AZStd::unordered_map<AZ::Name, AZ::EntityId> URDFMetadataComponent::GetHierarchy()
+    {
+        return m_hierarchyMap;
+    }
 
 } // namespace ROS2
